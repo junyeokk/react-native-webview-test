9a86b086c10c1193855cfac112d600c2ecc73b70
junyeokk <cjh4302@gmail.com>
2023-09-13 23:47:43 +0900

    refactor: 모바일 화면에 맞게 수정

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import * as React from "react";
 import { WebView } from "react-native-webview";
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
 import Constants from "expo-constants";
+
+const deviceHeight = Dimensions.get("window").height;
+const deviceWidth = Dimensions.get("window").width;
 
 export default function App() {
   return (
     <WebView
       style={styles.container}
-      source={{ uri: "https://junyeokk-dev.vercel.app" }}
+      source={{ uri: "http://localhost:5173" }}
     />
   );
 }
@@ -15,6 +18,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: "center",
     marginTop: Constants.statusBarHeight,
+  },
+  webview: {
+    flex: 1,
+    width: deviceWidth,
+    height: deviceHeight,
   },
 });
